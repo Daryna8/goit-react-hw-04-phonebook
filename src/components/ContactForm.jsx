1f6ae2895625fee19ef8ed49cb91c21ae54381d6
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-12-12 19:40:53 +0200

    fixed mistakes

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,60 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './PhoneBook.module.css';
 import { nanoid } from 'nanoid';
 
-export class ContactForm extends React.Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export const ContactForm = ({ handleAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     const id = nanoid();
-    const { name, number } = this.state;
     const newContact = { id, name, number };
 
-    this.props.handleAddContact(newContact);
+    handleAddContact(newContact);
 
-    this.setState({
-      name: '',
-      number: '',
-    });
+    setName('');
+    setNumber('');
   };
 
-  handleChangeInput = e => {
+  const handleChangeInput = e => {
     const { value, name } = e.currentTarget;
-    this.setState({ [name]: value });
+
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="">
-          Name
-          <input
-            value={name}
-            onChange={this.handleChangeInput}
-            type="text"
-            name="name"
-            required
-          />
-        </label>
-        <label htmlFor="">
-          Number
-          <input
-            value={number}
-            onChange={this.handleChangeInput}
-            type="tel"
-            name="number"
-            required
-          />
-        </label>
-        <button className={s.add_contanct_button} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="">
+        Name
+        <input
+          value={name}
+          onChange={handleChangeInput}
+          type="text"
+          name="name"
+          required
+        />
+      </label>
+      <label htmlFor="">
+        Number
+        <input
+          value={number}
+          onChange={handleChangeInput}
+          type="tel"
+          name="number"
+          required
+        />
+      </label>
+      <button className={s.add_contanct_button} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
